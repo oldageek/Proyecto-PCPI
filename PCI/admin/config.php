ffdc0c25f30c2543d9acfb0b3539c831341f0df9
Oldahir Gomez <oldahir_gomez25@hotmail.com>
2019-03-19 16:34:30 -0600

    Log In
    Inicio de la UI para el logeo de nuestros usuarios

---
+++ PCI/admin/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+$bd_config = array(
+    'basedatos' => 'dbppcpi',
+    'usuario' => 'root',
+    'pass' => ''
+);
+
+$pcpi_admin = array(
+    'usuario' => 'doctoramalu',
+    'password' => 'administradora2019'
+);
--- PCI/views/login.view.php
+++ PCI/views/login.view.php
@@ -1,9 +1,24 @@
 <?php require 'headerLogin.php'; ?>
 
     <div class="container">
-        <div class="row">
-            <div class="col">
+        <div class="row mt-5 mb-4">
+            <div class="col iconohome d-flex justify-content-end">
+                <a href="index.php" class="icono"><i class="fas fa-home"></i></a>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-6">
                 <h3>Aqui ira nuestro Log In</h3>
+            </div>
+            <div class="col-6 d-flex justify-content-center">
+                <div class="card w-80 text-center bg-light">
+                    <i class="card-img-top fas fa-user-circle"  alt="Card image cap"></i>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
