9cc15fe34728f2bb1e85ed4489d6064bcdf61904
Oldahir Gomez <oldahir_gomez25@hotmail.com>
2019-03-29 20:24:36 -0600

    diseño de UI
    Se creó el primer boceto del diseño para el usuario investigador

--- PCI/views/usuario.view.php
+++ PCI/views/usuario.view.php
@@ -6,14 +6,78 @@
             <img id="img-principal" src="img/bienvenidos.png" alt="Image not Found">
         </div>
     </div>
-    <div class="row d-flex justify-content-center">
+    <div class="row mb-4 d-flex justify-content-center">
         <div class="col-auto align-self-center">
             <h2>Guia de Navegación</h2>
         </div>
     </div>
     <div class="row">
-        <div class="col">
-            
+        <div class="col-4">
+            <img src="img/libros.png" style="width: 180px" alt="Image not found">
+        </div>
+        <div class="col-8">
+            <h5>Libros</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui libero rem, quod quasi voluptatum illum dolores tenetur amet consequuntur ipsa molestiae assumenda earum cum eligendi nisi dolore? Tempora, harum.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="img/capitulos.png" style="width: 180px" alt="Image not found">
+        </div>
+        <div class="col-8">
+            <h5>Capitulos</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui libero rem, quod quasi voluptatum illum dolores tenetur amet consequuntur ipsa molestiae assumenda earum cum eligendi nisi dolore? Tempora, harum.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="img/publicaciones.png" style="width: 180px" alt="Image not found">
+        </div>
+        <div class="col-8">
+            <h5>Publicaciones</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui libero rem, quod quasi voluptatum illum dolores tenetur amet consequuntur ipsa molestiae assumenda earum cum eligendi nisi dolore? Tempora, harum.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="img/revistas.png" style="width: 180px" alt="Image not found">
+        </div>
+        <div class="col-8">
+            <h5>Revistas</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui libero rem, quod quasi voluptatum illum dolores tenetur amet consequuntur ipsa molestiae assumenda earum cum eligendi nisi dolore? Tempora, harum.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="img/congresos.png" style="width: 180px" alt="Image not found">
+        </div>
+        <div class="col-8">
+            <h5>Congresos</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui libero rem, quod quasi voluptatum illum dolores tenetur amet consequuntur ipsa molestiae assumenda earum cum eligendi nisi dolore? Tempora, harum.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="img/Proyectos.png" style="width: 180px" alt="Image not found">
+        </div>
+        <div class="col-8">
+            <h5>Pryectos</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui libero rem, quod quasi voluptatum illum dolores tenetur amet consequuntur ipsa molestiae assumenda earum cum eligendi nisi dolore? Tempora, harum.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="img/tesis.png" style="width: 180px" alt="Image not found">
+        </div>
+        <div class="col-8">
+            <h5>Tesis</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui libero rem, quod quasi voluptatum illum dolores tenetur amet consequuntur ipsa molestiae assumenda earum cum eligendi nisi dolore? Tempora, harum.</p>
         </div>
     </div>
 </div>
