d625ac24b87d283d89229d52ba1e46e8a2b662d4
RacsoSK <37522775+RacsoSK@users.noreply.github.com>
2019-04-05 04:38:36 -0600

    Vistas de productos de investigacion del Usuario

--- PCI/views/misArticulos.view.php
+++ PCI/views/misArticulos.view.php
@@ -6,6 +6,64 @@
             <h2>Mis Articulos</h2>
         </div>
     </div>
+    
+    <div class="container d-flex" >           
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <!--th>idLibro</th>
+                    <th>idInvestigadorLibro</th-->
+                    <th>ISSN</th>
+                    <th>TITULO ARTICULO</th>
+                    <th>REVISTA</th>
+                    <th>NUMERO</th>
+                    <th>VOLUMEN</th>
+                    <th>AÑO PUBLICACIÓN</th>
+                    <th>PAGINA INICIO</th>
+                    <th>PAGINA FIN</th>
+                    <th>PALABRA 1</th>
+                    <th>PALABRA 2</th>
+                    <th>PALABRA 3</th>
+                    <th>AREA</th>
+                    <th>SUB AREA</th>
+                    <th>APOYO CONACYT</th>
+                    <th>AUTORES</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+                <?php 
+                
+                while($mostrar=mysqli_fetch_array($resultado)){
+                ?>
+         
+                    <tr>
+                        <td><?php echo utf8_encode($mostrar['issn']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['tituloArticulo']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['nombreRevista']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['numeroRevista']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['volumenRevista']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['anioPublicacion']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['paginaInicio']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['paginaFin']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['palabraClave1']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['palabraClave2']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['palabraClave3']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['area']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['subArea']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['apoyoConacyt']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['autores']) ?></td>
+                        
+                    </tr>
+                <?php
+                }
+                ?>
+                
+            </tbody>
+        </table>
+    </div>
+    
+    
     <div class="row">
         <div class="col-12">
             
--- PCI/views/misLibros.view.php
+++ PCI/views/misLibros.view.php
@@ -6,11 +6,77 @@
             <h2>Mis Libros</h2>
         </div>
     </div>
-    <div class="row">
-        <div class="col-12">
-            
-        </div>
+        
+    <div class="container d-flex" >           
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <!--th>idLibro</th>
+                    <th>idInvestigadorLibro</th-->
+                    <th>ISBN</th>
+                    <th>TITULO</th>
+                    <th>AÑO CREACION</th>
+                    <th>VOLUMEN</th>
+                    <th>TOMO</th>
+                    <th>EDICION</th>
+                    <th>EDITORIAL</th>
+                    <th>NUMERO DE PAGINAS</th>
+                    <th>FINANCIAMIENTO</th>
+                    <th>IMPORTE</th>
+                    <th>ESTATUS</th>
+                    <th>IDIOMA</th>
+                    <th>PALABRA 1</th>
+                    <th>PALABRA 2</th>
+                    <th>PALABRA 3</th>
+                    <th>AREA</th>
+                    <th>SUB AREA</th>
+                    <th>APOYO CONACYT</th>
+                    <th>AUTORES</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+                <?php 
+                
+                while($mostrar=mysqli_fetch_array($resultado)){
+                ?>
+         
+                    <tr>
+                        <td><?php echo utf8_encode($mostrar['isbn']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['titulo']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['anioCreacion']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['volumen']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['tomo']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['edicion']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['editorial']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['numPaginas']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['financiamiento']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['importeFinanciado']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['estatus']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['idioma']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['palabraClave1']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['palabraClave2']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['palabraClave3']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['area']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['subArea']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['apoyoConacyt']) ?></td>
+                        <td><?php echo utf8_encode($mostrar['Autores']) ?></td>
+                        
+                    </tr>
+                <?php
+                }
+                ?>
+                
+            </tbody>
+        </table>
     </div>
+    
+    
+    
+    
+    
+    
+    
     <div class="col d-flex justify-content-end">        
         <div>
             <button type="button"  class="btn btn-primary btn-lg"><a href="registroLibro.php" style="color:white;">+ Agregar Libro</a></button>
